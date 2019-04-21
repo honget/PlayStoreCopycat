7135f4ee9301bff7897e9542129a609d3a3fbb97
Hong <bilre84@naver.com>
2019-04-21 11:36:22 +0900

    app 데이터를 getView 에 적용

--- app/src/main/java/com/example/playstorecopycat/adapters/AppAdapter.java
+++ app/src/main/java/com/example/playstorecopycat/adapters/AppAdapter.java
@@ -1,13 +1,16 @@
 package com.example.playstorecopycat.adapters;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.icu.util.Calendar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.playstorecopycat.R;
+import com.example.playstorecopycat.databinding.AppItemListBinding;
 import com.example.playstorecopycat.datas.app;
 
 import java.util.List;
@@ -39,7 +42,29 @@ public class AppAdapter extends ArrayAdapter<app> {
 
         //실질적으로 상황에 맞게 머리를 써야 하는 부분
 
-//        row.findViewById("");
+        //앱 리스트에서 해당 줄의 데이터 뽑음
+        app appData = appList.get(position);
+
+        //아이디 찾기
+        TextView rankAndTitleTxtV = row.findViewById(R.id.rankAndTitleTxtV);
+        TextView companyNameTxtV = row.findViewById(R.id.companyNameTxtV);
+        TextView priceOrInstalledTxtV = row.findViewById(R.id.priceOrInstalledTxtV);
+
+        //등스와 제목 세팅
+        rankAndTitleTxtV.setText(String.format("%d, %s", appData.getRank(), appData.getTitle()));
+
+        //회사 이름은 그대로
+        companyNameTxtV.setText(appData.getCompanyName());
+
+        //설치 여부 따라 안내
+        if(!appData.isMine()){
+            //미설치
+            //Stirng.format의 %,d 를 이용해 세자리 마다 콤파 적용
+            priceOrInstalledTxtV.setText(String.format("%,d 원"), appDaa.getPrice());
+        }
+        //설치 코딩한 글자 그대로라 주석처리
+//        else {
+//        }
 
         return row;
     }
