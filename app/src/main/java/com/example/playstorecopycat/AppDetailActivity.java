67e161ab1290e87014475cb583094adc33aa3891
Hong <bilre84@naver.com>
2019-04-28 10:28:42 +0900

    sms 발송 기능

--- app/src/main/java/com/example/playstorecopycat/AppDetailActivity.java
+++ app/src/main/java/com/example/playstorecopycat/AppDetailActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 import com.example.playstorecopycat.databinding.ActivityAppDetailBinding;
 import com.example.playstorecopycat.datas.Appli;
 
+import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
@@ -28,6 +29,9 @@ public class AppDetailActivity extends AppCompatActivity {
 
     int hour = 3;
     int min = 15;
+    int mYear = 2019;
+    int mMonth = 03;
+    int mDayOfMonth= 27;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,7 +75,8 @@ public class AppDetailActivity extends AppCompatActivity {
 
                 Uri phoneUri =  Uri.parse("tel:010-1234-5678");
 
-                Intent intent = new Intent(Intent.ACTION_DIAL, phoneUri);
+                Intent intent = new Intent(Intent.ACTION_CALL, phoneUri);
+//                Intent intent = new Intent(Intent.ACTION_DIAL, phoneUri);
 
                 startActivity(intent);
 
@@ -81,7 +86,14 @@ public class AppDetailActivity extends AppCompatActivity {
         act.smsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                Uri smsUri = Uri.parse("smsto:010-1234-5678");
+                Uri smsUri = Uri.parse("smsto:01012345678");
 
+                Intent intent = new Intent(Intent.ACTION_SENDTO, smsUri);
+
+                intent.putExtra("sms_body", "미리 작성된 메세지");
+
+                startActivity(intent);
             }
         });
 
@@ -92,48 +104,112 @@ public class AppDetailActivity extends AppCompatActivity {
             }
         });
 
+//        act.dateTxt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("텍스트 뷰 클릭!", "동작 확인용!!!!!");
+//
+//                //날짜
+//                DatePickerDialog dpd = new DatePickerDialog(AppDetailActivity.this, new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//
+//                        //Toast.makeText(AppDetailActivity.this, String.format("%d년 %월 %일 ", year, month + 1, dayOfMonth), Toast.LENGTH_SHORT);
+//
+//                        //new Calendar 라고 만들지 않는다. 싱글턴 패턴의 일종
+//                        Calendar cal = Calendar.getInstance();
+//
+//                        /**
+//                         * 같은 메소드인데 값에 따라 행동 다름 => overloading 의 예시
+//                         */
+//                        //1. 항목별  세팅
+//                        cal.set(Calendar.YEAR, year);
+//                        cal.set(Calendar.MONTH, month);
+//                        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+//
+//                        //2. 일괄 세팅
+//                        cal.set(year, month, dayOfMonth);
+//
+//                        //cal 에 저장된 정보를 출력
+//                        //날짜를 양식으로 바꾸고 싶을때  SimpleDateFormat 사용
+//
+//                        //어떤 양식으로 문자를 출력할지 양식 지정
+//                        SimpleDateFormat sdf = new SimpleDateFormat("YYYY년 M월 d일");
+//
+//                        //양식으로 String 출력
+//                        String dataSrt = sdf.format(cal.getTimeInMillis());
+//
+//                        //화면에 출력
+//                        act.dateTxt.setText(dataSrt);
+//                    }
+//                    //초기 출력 값
+//                }, 2019, 4 - 1, 27);
+//
+//                dpd.show();
+//            }
+//        });
+//
+//        act.timeTxt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                TimePickerDialog tpd = new TimePickerDialog(AppDetailActivity.this, new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//
+//                        hour = hourOfDay;
+//                        min = minute;
+//
+//                        Calendar cal = Calendar.getInstance();
+//
+//                        cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+//                        cal.set(Calendar.MINUTE, minute);
+//
+//                        SimpleDateFormat sdf = new SimpleDateFormat("a h시 m분", Locale.KOREA);
+//
+//                        String timeStr = sdf.format(cal.getTimeInMillis());
+//
+//                        act.timeTxt.setText(timeStr);
+//
+//
+//                    }
+//                }, hour, min, true);
+//
+//                tpd.show();
+//            }
+//        });
+
+
+
+
+
+
+
+        //연습 문제, 날짜를 누르고 선택하면 반영되기
+        //시간을 누르면 선택하면 반영 되게 ,AM /PM을 오전/ 오후로 변경
+
         act.dateTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("텍스트 뷰 클릭!", "동작 확인용!!!!!");
-
-                //날짜
                 DatePickerDialog dpd = new DatePickerDialog(AppDetailActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
-                        //Toast.makeText(AppDetailActivity.this, String.format("%d년 %월 %일 ", year, month + 1, dayOfMonth), Toast.LENGTH_SHORT);
+                        mYear = year;
+                        mMonth = month;
+                        mDayOfMonth = dayOfMonth;
 
-                        //new Calendar 라고 만들지 않는다. 싱글턴 패턴의 일종
                         Calendar cal = Calendar.getInstance();
 
-                        /**
-                         * 같은 메소드인데 값에 따라 행동 다름 => overloading 의 예시
-                         */
-                        //1. 항목별  세팅
-                        cal.set(Calendar.YEAR, year);
-                        cal.set(Calendar.MONTH, month);
-                        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-
-                        //2. 일괄 세팅
                         cal.set(year, month, dayOfMonth);
 
-                        //cal 에 저장된 정보를 출력
-                        //날짜를 양식으로 바꾸고 싶을때  SimpleDateFormat 사용
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 M월 d일");
 
-                        //어떤 양식으로 문자를 출력할지 양식 지정
-                        SimpleDateFormat sdf = new SimpleDateFormat("YYYY년 M월 d일");
+                        act.dateTxt.setText(sdf.format(cal.getTimeInMillis()));
 
-                        //양식으로 String 출력
-                        String dataSrt = sdf.format(cal.getTimeInMillis());
-
-                        //화면에 출력
-                        act.dateTxt.setText(dataSrt);
                     }
-                    //초기 출력 값
-                }, 2019, 4 - 1, 27);
+                }, mYear, mMonth, mDayOfMonth);
 
-                dpd.show();
+                dpd.show();;
             }
         });
 
@@ -152,19 +228,18 @@ public class AppDetailActivity extends AppCompatActivity {
                         cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
                         cal.set(Calendar.MINUTE, minute);
 
-                        SimpleDateFormat sdf = new SimpleDateFormat("a h시 m분", Locale.KOREA);
+                        SimpleDateFormat sdf = new SimpleDateFormat("a h 시 m분", Locale.KOREA);
 
-                        String timeStr = sdf.format(cal.getTimeInMillis());
-
-                        act.timeTxt.setText(timeStr);
-
+                        act.timeTxt.setText(sdf.format(cal.getTimeInMillis()));
 
                     }
-                }, hour, min, true);
+                }, hour, min, false);
 
                 tpd.show();
             }
         });
+
+
 
     }
 }
